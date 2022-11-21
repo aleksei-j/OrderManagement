44301acfbdd2fece6a14d58b8c7a176a860a8f56
Aleksei <rololik.1@gmail.com>
2022-11-21 02:49:40 +0200

    resolving merge issues

--- src/main/java/com/alekseij/OrderManagement/service/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.alekseij.OrderManagement.service;
-
-import com.alekseij.OrderManagement.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
--- src/main/java/com/alekseij/OrderManagement/service/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.alekseij.OrderManagement.service;
-
-import com.alekseij.OrderManagement.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
